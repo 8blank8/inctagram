b9b67bedcd161f38198d81975c4611a008c312de
8blank8 <roc.32@yandex.ru>
2023-10-16 23:59:22 +0700

    fix test app.controller

--- apps/inctagram/src/app.controller.spec.ts
+++ apps/inctagram/src/app.controller.spec.ts
@@ -18,8 +18,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World! Deployed"', () => {
+      expect(appController.getHello()).toBe('Hello World! Deployed');
     });
   });
 });
