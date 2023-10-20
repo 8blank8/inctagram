188d91c1f6a75bd703be388be19f14f77bbaffdf
Benedikt89 <constantine.kostka@gmail.com>
2023-10-20 17:33:53 +0200

    feat: build with webpack

--- apps/inctagram/src/app.module.ts
+++ apps/inctagram/src/app.module.ts
@@ -7,9 +7,11 @@ import { PostService } from './post/post.service';
 import { PrismaService } from './prisma.service';
 import { FilesController } from '../../files/src/files.controller';
 import { FilesService } from '../../files/src/files.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ClientsModule.register([
       {
         name: 'FILE_SERVICE',
