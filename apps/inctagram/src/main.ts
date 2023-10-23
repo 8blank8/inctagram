1cca1d41c823365aefe2a7b3a44b766f3cd6a9f2
Benedikt89 <constantine.kostka@gmail.com>
2023-10-23 18:05:14 +0200

    feat: login dto

--- apps/inctagram/src/main.ts
+++ apps/inctagram/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+    },
+  });
 
   // Adding global prefix
   app.setGlobalPrefix('api/v1');
