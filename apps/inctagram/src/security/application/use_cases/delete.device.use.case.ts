4c33cffeba248b5c41ed18c9dd319767c7c0d9b7
8blank8 <roc.32@yandex.ru>
2023-10-24 18:53:12 +0700

    changed typeorm in prisma

--- apps/inctagram/src/security/application/use_cases/delete.device.use.case.ts
+++ apps/inctagram/src/security/application/use_cases/delete.device.use.case.ts
@@ -1,11 +1,7 @@
 import { CommandHandler } from "@nestjs/cqrs";
-import { SecurityRepository } from "../../infrastructure/mongoose/security.repository";
-import { SecurityQueryRepository } from "../../infrastructure/mongoose/security.query.repository";
 import { ForbiddenException } from "@nestjs/common";
-import { SecurityQueryRepositorySql } from "../../infrastructure/sql/security.query.repository.sql";
-import { SecurityRepositorySql } from "../../infrastructure/sql/security.repository.sql";
-import { SecurityRepositoryTypeorm } from "../../infrastructure/security.repository.typeorm";
-import { SecurityQueryRepositoryTypeorm } from "../../infrastructure/secutity.query.repository.typeorm";
+import { SecurityRepository } from "../../repository/security.repository";
+import { SecurityQueryRepository } from "../../repository/secutity.query.repository";
 
 
 export class DeleteDeviceCommand {
@@ -18,10 +14,8 @@ export class DeleteDeviceCommand {
 @CommandHandler(DeleteDeviceCommand)
 export class DeleteDeviceUseCase {
     constructor(
-        // private securityRepository: SecurityRepository,
-        // private securityQueryRepository: SecurityQueryRepository
-        private securityRepository: SecurityRepositoryTypeorm,
-        private securityQueryRepository: SecurityQueryRepositoryTypeorm
+        private securityRepository: SecurityRepository,
+        private securityQueryRepository: SecurityQueryRepository
     ) { }
 
     async execute(command: DeleteDeviceCommand): Promise<boolean> {
@@ -31,7 +25,7 @@ export class DeleteDeviceUseCase {
         const device = await this.securityQueryRepository.findDeviceById(deviceId)
         if (!device) return false
 
-        if (device.user.id !== userId) throw new ForbiddenException()
+        if (device.userId !== userId) throw new ForbiddenException()
 
         await this.securityRepository.deleteDeviceById(device.id)
 
