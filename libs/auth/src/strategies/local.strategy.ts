09d30be86d6c968e112b3daea0924ba1a5ef6de2
8blank8 <roc.32@yandex.ru>
2023-10-24 18:57:46 +0700

    add local strategy

---
+++ libs/auth/src/strategies/local.strategy.ts
@@ -0,0 +1,24 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+import { ValidateUserCommand } from '@app/main/auth/use_cases/validate.user.use.case';
+
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        private commandBus: CommandBus
+    ) {
+        super({ usernameField: 'loginOrEmail' });
+    }
+
+    async validate(username: string, password: string): Promise<any> {
+        const user = await this.commandBus.execute(new ValidateUserCommand(username, password));
+
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+        return user;
+    }
+}
