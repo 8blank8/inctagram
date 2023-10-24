d23c27af57ef94618d7e307f2c19d94160f5d8d8
8blank8 <roc.32@yandex.ru>
2023-10-24 18:56:49 +0700

    add dropDatabase

--- apps/inctagram/test/utils/drop.database.ts
+++ apps/inctagram/test/utils/drop.database.ts
@@ -1,4 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+
 
 export const dropDataBase = async (app) => {
+    const tableNames = ['User', 'Post', 'Device'];
 
+
+    const prisma = new PrismaClient();
+    async function main() {
+        for (const tableName of tableNames) await prisma.$queryRawUnsafe(`Truncate "${tableName}" restart identity cascade;`);
+    }
+
+    main().finally(async () => {
+        await prisma.$disconnect();
+    });
 }
