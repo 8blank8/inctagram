927ab73578bce904f9d4d5fa42b55a881f16668e
8blank8 <roc.32@yandex.ru>
2023-10-24 18:42:54 +0700

    add modules and localStrategy

--- apps/inctagram/src/app.module.ts
+++ apps/inctagram/src/app.module.ts
@@ -2,18 +2,30 @@ import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from '@app/db';
+import { PassportModule } from '@nestjs/passport';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserService } from './user/user.service';
 import { PostService } from './post/post.service';
+import { JwtModule } from '@nestjs/jwt';
+import { CqrsModule } from '@nestjs/cqrs';
+
 
 import { FilesController } from '../../files/src/files.controller';
 import { FilesService } from '../../files/src/files.service';
+import { LocalStrategy } from '@app/auth';
+import { AuthModule } from './auth/auth.module';
+import { SecurityModule } from './security/security.module';
+import { UserModule } from './user/user.module';
+
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    PassportModule,
+    JwtModule.register({}),
+    CqrsModule,
     ClientsModule.register([
       {
         name: 'FILE_SERVICE',
@@ -26,6 +38,9 @@ import { FilesService } from '../../files/src/files.service';
         },
       },
     ]),
+    AuthModule,
+    SecurityModule,
+    UserModule
   ],
   controllers: [AppController, FilesController],
   providers: [
@@ -34,6 +49,7 @@ import { FilesService } from '../../files/src/files.service';
     PostService,
     PrismaService,
     FilesService,
+    LocalStrategy,
   ],
 })
 export class AppModule {}
