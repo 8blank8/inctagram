fba15495a9431e20fc256e80400059b9272ebbbd
Benedikt89 <constantine.kostka@gmail.com>
2023-10-27 18:18:14 +0200

    feat: fixed test

--- apps/inctagram/src/auth/auth.service.spec.ts
+++ apps/inctagram/src/auth/auth.service.spec.ts
@@ -2,13 +2,21 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { PrismaService } from '@app/db';
 import { JwtService } from '@nestjs/jwt';
+import { UserRepository } from '@app/main/user/repository/user.repository';
+import { UserQueryRepository } from '@app/main/user/repository/user.query.repository';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService, PrismaService, JwtService],
+      providers: [
+        AuthService,
+        UserRepository,
+        PrismaService,
+        UserQueryRepository,
+        JwtService,
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
