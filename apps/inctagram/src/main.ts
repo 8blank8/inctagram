14bd6408f553d832d516f2b3fac45e570ca4da07
Benedikt89 <constantine.kostka@gmail.com>
2023-10-27 18:42:10 +0200

    feat: added cors

--- apps/inctagram/src/main.ts
+++ apps/inctagram/src/main.ts
@@ -9,6 +9,17 @@ async function bootstrap() {
       origin: '*',
     },
   });
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:5173',
+      'http://localhost:8080',
+      'https://incubator-icta-trainee.uk/',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   // Adding global prefix
   app.setGlobalPrefix('api/v1');
