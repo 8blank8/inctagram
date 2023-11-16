e19a8efefc2eae2e53188614e57766ff4c57e458
8blank8 <roc.32@yandex.ru>
2023-11-16 20:49:32 +0700

    add: tests for user.e2e.spec.ts

--- apps/inctagram/test/user.e2e.spec.ts
+++ apps/inctagram/test/user.e2e.spec.ts
@@ -2,12 +2,13 @@ import { HttpStatus, INestApplication } from '@nestjs/common';
 import { mockMailCodes, startTestConfig } from './utils/start.app';
 import { testUsers } from './utils/test.users.ustil';
 import * as request from 'supertest';
+import * as querystring from 'querystring';
 
 describe('User', () => {
   let app: INestApplication;
 
   const [user2] = testUsers;
-  //   let user2Token = null;
+  let user2Token = null;
   let server;
 
   beforeAll(async () => {
@@ -30,6 +31,14 @@ describe('User', () => {
         });
     });
 
+    it('should send code', async () => {
+      const data = querystring.parse(mockMailCodes[user2.email]);
+      await request(app.getHttpServer())
+        .post(`/auth/confirm-code`)
+        .send(data)
+        .expect(HttpStatus.OK);
+    });
+
     it('should login with right data', async () => {
       await request(server)
         .post(`/auth/login`)
@@ -37,8 +46,8 @@ describe('User', () => {
         .send({ email: user2.email, password: user2.password })
         .expect(HttpStatus.OK)
         .then((res) => {
-          console.log({ token_body: res.body });
-          //   user2Token = res.body?.token?.accessToken || '';
+          // console.log({ token_body: res.body });
+          user2Token = res.body.accessToken || '';
           expect(res.body).toEqual({
             accessToken: expect.any(String),
             refreshToken: expect.any(String),
@@ -46,36 +55,49 @@ describe('User', () => {
         });
     });
 
-    // it('should be status 204 correct inputData', async () => {
-    //     console.log({ token: user2Token })
-    //     const res = await request(server)
-    //         .put('/user/profile')
-    //         .set('Authorization', `Bearer ${user2Token}`)
-    //         .send({
-    //             username: 'user1username',
-    //             firstname: 'Biba',
-    //             lastname: 'Boba',
-    //             dateOfBirth: '12.12.2000',
-    //             aboutMe: 'about me info',
-    //         });
+    it('should be status 400 dateOfBirth <= 13', async () => {
+      const res = await request(server)
+        .put('/user/profile')
+        .set('Authorization', `Bearer ${user2Token}`)
+        .send({
+          username: 'user1username',
+          firstname: 'Biba',
+          lastname: 'Boba',
+          dateOfBirth: '12.12.2020',
+          aboutMe: 'about me info',
+        });
+      expect(res.status).toBe(HttpStatus.BAD_REQUEST);
+    });
 
-    //     expect(res.status).toBe(HttpStatus.NO_CONTENT);
-    // });
+    it('should be status 204 correct inputData', async () => {
+      const res = await request(server)
+        .put('/user/profile')
+        .set('Authorization', `Bearer ${user2Token}`)
+        .send({
+          username: 'user1username',
+          firstname: 'Biba',
+          lastname: 'Boba',
+          dateOfBirth: '12.12.2000',
+          aboutMe: 'about me info',
+        });
+      expect(res.status).toBe(HttpStatus.NO_CONTENT);
+    });
 
-    // it('find my profile after changed', async () => {
-    //     const res = await request(server)
-    //         .get('/user/profile')
-    //         .set('Authorization', `Bearer ${user2Token}`);
+    it('find my profile after changed', async () => {
+      const res = await request(server)
+        .get('/user/profile')
+        .set('Authorization', `Bearer ${user2Token}`);
 
-    //     expect(res.status).toBe(HttpStatus.OK);
-    //     expect(res.body).toEqual({
-    //         username: 'user1username',
-    //         firstname: 'Biba',
-    //         lastname: 'Boba',
-    //         dateOfBirth: '12.12.2000',
-    //         aboutMe: 'about me info',
-    //     });
-    // });
+      expect(res.status).toBe(HttpStatus.OK);
+      expect(res.body).toEqual({
+        id: expect.any(String),
+        userId: expect.any(String),
+        firstName: 'Biba',
+        familyName: 'Boba',
+        dateOfBirth: '12.12.2000',
+        aboutMe: 'about me info',
+      });
+    });
   });
 
   afterAll(async () => {
