6c7670f0270fd0892df18bdcd439c5f2106c3bc4
8blank8 <roc.32@yandex.ru>
2023-12-06 22:29:31 +0700

    feat: add Buffer

--- apps/files/src/upload/upload.service.ts
+++ apps/files/src/upload/upload.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { readFileSync, unlinkSync, writeFile } from 'fs';
+// import { readFileSync, unlinkSync, writeFile } from 'fs';
 import * as sharp from 'sharp';
 import { ConfigService } from '@nestjs/config';
 import {
@@ -20,6 +20,7 @@ interface FilePayload {
   fileName: string;
   authorId: string;
   prefix: FolderType;
+  imageBuffer: Buffer;
 }
 
 @Injectable()
@@ -37,10 +38,10 @@ export class UploadService {
     private filesRepository: FilesRepository,
   ) {}
 
-  async resize(filePath: string) {
+  async resize(imageBuffer: Buffer) {
     // resizing part started from here
-    const base64 = readFileSync(filePath);
-    const img = sharp(base64, {});
+    // const base64 = readFileSync(filePath);
+    const img = sharp(imageBuffer);
     let metadata = await img.metadata();
     let buffer;
     if (metadata.width > 800) {
@@ -53,17 +54,24 @@ export class UploadService {
   }
 
   async uploadFile(payload: FilePayload) {
-    const { filePath, mimetype, fileName, prefix, authorId } = payload;
+    const {
+      //  filePath,
+      mimetype,
+      fileName,
+      prefix,
+      authorId,
+      imageBuffer,
+    } = payload;
     const storePath = [prefix, authorId, fileName].join('/');
     try {
-      const { buffer, metadata } = await this.resize(filePath);
-      writeFile(filePath, buffer, 'binary', async (error) => {
-        if (!error) {
-          await this.uploadToS3(filePath, storePath, mimetype);
-        } else {
-          return { message: 'uploaded unsuccessfully' };
-        }
-      });
+      const { buffer, metadata } = await this.resize(imageBuffer);
+      // writeFile(filePath, buffer, 'binary', async (error) => {
+      //   if (!error) {
+      await this.uploadToS3(buffer, storePath, mimetype);
+      //   } else {
+      //     return { message: 'uploaded unsuccessfully' };
+      //   }
+      // });
       const fileData = {
         url: `https://inctagram-trainee.s3.eu-central-1.amazonaws.com/${storePath}`,
         authorId: authorId,
@@ -72,7 +80,7 @@ export class UploadService {
       };
       return await this.filesRepository.saveFileData(fileData, prefix);
     } catch (err) {
-      unlinkSync(filePath);
+      // unlinkSync(filePath);
       console.log('[SERVER ERROR][UploadToS3Service:uploadFile]: ', err);
       throw err;
     }
@@ -101,17 +109,17 @@ export class UploadService {
     return { message: 'deleted unsuccessfully' };
   }
 
-  private async uploadToS3(filePath: any, fileName: string, fileType: string) {
-    const blob = readFileSync(filePath);
+  private async uploadToS3(buffer: Buffer, fileName: string, fileType: string) {
+    // const blob = readFileSync(filePath);
     const uploadedImage: PutObjectCommandOutput = await this.s3Client.send(
       new PutObjectCommand({
         Bucket: this.configService.getOrThrow('AWS_S3_BUCKET_NAME'),
         Key: fileName,
-        Body: blob,
+        Body: buffer,
         ContentType: fileType,
       }),
     );
-    unlinkSync(filePath);
+    // unlinkSync(filePath);
     return uploadedImage;
   }
 }
