541fef1c6484055e576689e380d241626822a5a0
8blank8 <roc.32@yandex.ru>
2023-12-06 22:28:00 +0700

    feat: add test

--- apps/files/src/upload/upload.controller.spec.ts
+++ apps/files/src/upload/upload.controller.spec.ts
@@ -4,6 +4,8 @@ import { UploadService } from './upload.service';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from '@app/db';
 import { FilesRepository } from './repository/files.repository';
+// import { readFile } from 'node:fs/promises';
+// import { Readable } from 'node:stream';
 
 describe('UploadController', () => {
   let controller: UploadController;
@@ -20,4 +22,34 @@ describe('UploadController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  // it('upload file', async () => {
+  //   const f: Buffer = await readFile('./apps/files/src/upload/10.jpg')
+
+  //   const file: Express.Multer.File = {
+  //     fieldname: 'file',
+  //     originalname: 'file.txt',
+  //     encoding: '7bit',
+  //     mimetype: 'text/plain',
+  //     destination: 'destination/path',
+  //     filename: '10.jpg',
+  //     // path: localFilePath,
+  //     size: 12345,
+  //     stream: new Readable,
+  //     path: '',
+  //     buffer: f
+  //   }
+  //   controller.uploadFile({
+  //     file,
+  //     user: {
+  //       id: '',
+  //       email: '',
+  //       createdAt: undefined,
+  //       updatedAt: undefined,
+  //       username: '',
+  //       emailConfirmed: false,
+  //       password: ''
+  //     }
+  //   })
+  // })
 });
