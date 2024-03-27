91de276ba1e2d8e96125477ce0f80ae2cfa51a84
8blank8 <springfield.3298@gmail.com>
2024-03-28 01:21:33 +0700

    update: import for PostController

--- apps/inctagram/src/post/post.module.ts
+++ apps/inctagram/src/post/post.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from '@app/db';
 import { PostService } from '@app/main/post/post.service';
-import { PostController } from '@app/main/post/post.controller';
+import { PostController } from './controller/post.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
