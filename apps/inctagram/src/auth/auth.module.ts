4d4a5feaec4eabcac7f33dd9e9a343ec17bbe3c7
8blank8 <springfield.3298@gmail.com>
2024-04-03 15:12:33 +0700

    update: auth.module

--- apps/inctagram/src/auth/auth.module.ts
+++ apps/inctagram/src/auth/auth.module.ts
@@ -5,18 +5,23 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { ConfigModule } from '@nestjs/config';
 
 import { MailService } from '@app/common';
-import { RegisterGoogleUserUseCase } from '@app/main/auth/use_cases/register-google-user.use-case';
-import { RegisterGithubUserUseCase } from '@app/main/auth/use_cases/register-github-user.use-case';
-import { CreateDeviceUseCase } from '@app/main/security/use_cases/create.device.use.case';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './contoller/auth.controller';
-import { AuthorizeUserUseCase } from './use_cases/authorize-user.use-case';
-import { ValidateUserUseCase } from './use_cases/validate.user.use.case';
+import { AuthorizeUserUseCase } from './use_cases/login/authorize-user.use-case';
+import { ValidateUserUseCase } from './use_cases/login/validate.user.use.case';
 import { SecurityModule } from '../security/security.module';
 import { UserModule } from '../user/user.module';
-import { LogOutUserUseCase } from '@app/main/auth/use_cases/log-out.use-case';
-import { DeleteDeviceUseCase } from '@app/main/security/use_cases/delete-device.use-case';
+import { CreateDeviceUseCase } from '../security/use_cases/create/create.device.use.case';
+import { RegisterGithubUserUseCase } from './use_cases/github/register-github-user.use-case';
+import { RegisterGoogleUserUseCase } from './use_cases/google/register-google-user.use-case';
+import { LogOutUserUseCase } from './use_cases/logout/log-out.use-case';
+import { DeleteDeviceUseCase } from '../security/use_cases/delete/delete-device.use-case';
+import { EmailConfirmationUseCase } from '../user/use_cases/email/email-confirmation.use-case';
+import { ResendConfirmationCodeUseCase } from '../user/use_cases/email/resend-confirmation-code.use-case';
+import { PasswordResetMailUseCase } from '../user/use_cases/password/password-reset-email.use-case';
+import { ResetUserPasswordUseCase } from '../user/use_cases/password/reset-user-password.use-case';
+import { CreateUserUseCase } from '../user/use_cases/registration/create-user.use-case';
 
 @Module({
   imports: [
@@ -38,7 +43,12 @@ import { DeleteDeviceUseCase } from '@app/main/security/use_cases/delete-device.
     RegisterGoogleUserUseCase,
     LogOutUserUseCase,
     DeleteDeviceUseCase,
+    EmailConfirmationUseCase,
+    ResendConfirmationCodeUseCase,
+    PasswordResetMailUseCase,
+    ResetUserPasswordUseCase,
+    CreateUserUseCase,
   ],
-  exports: [AuthService],
+  exports: [AuthService, ValidateUserUseCase],
 })
 export class AuthModule {}
