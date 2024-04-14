d23ac1b4c8c8613978d9018a546b03312a16cef3
8blank8 <springfield.3298@gmail.com>
2024-04-14 22:31:15 +0700

    ...

--- libs/infra/postgres-ormconfig.ts
+++ libs/infra/postgres-ormconfig.ts
@@ -10,16 +10,16 @@ export const allEntities = [
     DeviceEntity
 ]
 
-const isTest = process.env.MODE === 'develop';
-console.log('=============================> database name', !isTest ? 'neondb' : process.env.PG_DATABASE)
+const isTest = process.env.MODE !== 'production';
+console.log('=============================> database name', isTest ? 'neondb' : process.env.PG_DATABASE)
 export const primaryPostgresConnectionOptions: PostgresConnectionOptions = {
     name: "default",
     type: 'postgres',
-    host: !isTest ? 'ep-twilight-wave-02964973.eu-central-1.aws.neon.tech' : process.env.PG_HOST,
-    port: !isTest ? 5432 : +process.env.PG_PORT,
-    username: !isTest ? 'springfield.3298' : process.env.PG_USER,
-    password: !isTest ? 'VRirOjE9BfN2' : process.env.PG_PASSWORD,
-    database: !isTest ? 'neondb' : process.env.PG_DATABASE,
+    host: isTest ? 'ep-twilight-wave-02964973.eu-central-1.aws.neon.tech' : process.env.PG_HOST,
+    port: isTest ? 5432 : +process.env.PG_PORT,
+    username: isTest ? 'springfield.3298' : process.env.PG_USER,
+    password: isTest ? 'VRirOjE9BfN2' : process.env.PG_PASSWORD,
+    database: isTest ? 'neondb' : process.env.PG_DATABASE,
     entities: allEntities,
     synchronize: false,
     logging: false,
