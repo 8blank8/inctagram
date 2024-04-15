e7acb0ee07f8f1d213fe7409ce8a4fb62a21954e
8blank8 <springfield.3298@gmail.com>
2024-04-16 01:57:17 +0700

    update: remove transaction method, added manager

--- apps/inctagram/src/modules/device/use-cases/create/create-device.use-case.ts
+++ apps/inctagram/src/modules/device/use-cases/create/create-device.use-case.ts
@@ -2,55 +2,33 @@ import { Injectable } from "@nestjs/common";
 import { CreateDeviceCommand } from "./dto/create-device.command";
 import { DeviceEntity } from "../../entities/device.entity";
 import { Result } from "../../../../../../../libs/core/result";
-import { UserRepository } from "../../../user/repository/user.repository";
-import { TransactionDecorator } from "../../../../infra/inside-transaction";
-import { DataSource, EntityManager } from "typeorm";
+import { EntityManager } from "typeorm";
+import { UserEntity } from "@inctagram/src/modules/user/entities/user.entity";
 
 
 @Injectable()
 export class CreateDeviceUseCase {
-    constructor(
-        private userRepo: UserRepository,
-        private dataSource: DataSource
-    ) { }
+    async execute(command: CreateDeviceCommand, manager: EntityManager): Promise<Result<DeviceEntity>> {
+        const { title, userId } = command
 
-    async execute(command: CreateDeviceCommand): Promise<Result<DeviceEntity>> {
-        const transaction = new TransactionDecorator(this.dataSource)
+        const user = await manager.findOneBy(UserEntity, { id: userId })
+        if (!user) return Result.Err('CreateDeviceUseCase: user not found')
 
-        return transaction.doOperation(
-            command,
-            this.doOperation.bind(this)
-        )
-    }
+        const device = new DeviceEntity()
+        device.createdAt = new Date()
+        device.title = title
+        device.user = user
 
-    async doOperation(
-        { title, userId }: CreateDeviceCommand,
-        manager: EntityManager
-    ): Promise<Result<DeviceEntity>> {
-        try {
+        await manager.save(device)
 
-            const user = await this.userRepo.getUserById(userId)
-            if (!user) return Result.Err('CreateDeviceUseCase: user not found')
-
-            const device = new DeviceEntity()
-            device.createdAt = new Date()
-            device.title = title
-            device.user = user
-
-            await manager.save(device)
-
-            if (!user.devices) {
-                user.devices = [device]
-            } else {
-                user.devices.push(device)
-            }
-
-            await manager.save(user)
-
-            return Result.Ok(device)
-        } catch (e) {
-            console.log(e)
-            return Result.Err('CreateDeviceUseCase: device not created')
+        if (!user.devices) {
+            user.devices = [device]
+        } else {
+            user.devices.push(device)
         }
+
+        await manager.save(user)
+
+        return Result.Ok(device)
     }
 }
