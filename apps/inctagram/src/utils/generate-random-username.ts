f406e675313c47625ec6c41ab15a10ca953c76f0
8blank8 <springfield.3298@gmail.com>
2024-04-16 01:58:26 +0700

    update: genereration unique username

--- apps/inctagram/src/utils/generate-random-username.ts
+++ apps/inctagram/src/utils/generate-random-username.ts
@@ -1,15 +1,5 @@
 export const generateUniqueUsername = () => {
-    var minLength = 6;
-    var maxLength = 30;
-    var characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_';
-    var username = '';
-
-    var usernameLength = Math.floor(Math.random() * (maxLength - minLength + 1)) + minLength;
-    username = '';
-    for (var i = 0; i < usernameLength; i++) {
-        var randomIndex = Math.floor(Math.random() * characters.length);
-        username += characters[randomIndex];
-    }
-
-    return username;
-}
+    const timestamp = new Date().getTime();
+    const randomSuffix = Math.floor(Math.random() * 100);
+    return `user_${timestamp}_${randomSuffix}`;
+};
