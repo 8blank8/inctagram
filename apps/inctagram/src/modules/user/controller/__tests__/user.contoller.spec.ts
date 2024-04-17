cf6003954cecf2de06a071c8aa2db46bd8f6cb73
8blank8 <springfield.3298@gmail.com>
2024-04-17 15:17:38 +0700

    add: user.contoller.spec

---
+++ apps/inctagram/src/modules/user/controller/__tests__/user.contoller.spec.ts
@@ -0,0 +1,182 @@
+import { HttpStatus, INestApplication } from "@nestjs/common"
+import { EntityManager, QueryRunner } from "typeorm"
+import { TestUtils } from "../../../../../../../libs/tests/test-utils"
+import * as request from 'supertest'
+import { TestSeeder } from "../../../../../../../libs/tests/test-seeder"
+import { UpdateUserDto } from "../../dto/input/update-user.dto"
+import { JwtService } from "@nestjs/jwt"
+import { TestingModule } from "@nestjs/testing"
+import { createJwtTokens } from "@inctagram/src/utils/create-tokens"
+import { UserEntity } from "../../entities/user.entity"
+import { createAndConfigureAppForE2eTests } from "@libs/tests/create-and-configure-app-for-e2e"
+
+
+describe('auth', () => {
+    let app: INestApplication
+    let _httpServer
+    let _queryRunner: QueryRunner
+    let manager: EntityManager
+    let moduleRef: TestingModule
+
+    let jwtService: JwtService
+
+    let testSeeder: TestSeeder
+
+    beforeAll(async () => {
+
+        ({
+            httpServer: _httpServer,
+            app: app,
+            queryRunner: _queryRunner,
+            manager: manager,
+            moduleRef
+        } = await createAndConfigureAppForE2eTests())
+        testSeeder = new TestSeeder(manager)
+
+        jwtService = moduleRef.get<JwtService>(JwtService)
+
+        await app.init()
+
+        await TestUtils.dropDb(_queryRunner)
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    beforeEach(async () => {
+        await TestUtils.dropDb(_queryRunner)
+    })
+
+    describe('user', () => {
+
+        it('update user is success', async () => {
+            const user = await testSeeder.createUser(testSeeder.getUserDto())
+            const accessToken = (await createJwtTokens(jwtService, user.id, 'asd')).accessToken
+
+            const dto: UpdateUserDto = {
+                username: user.username,
+                firstname: "Abra",
+                lastname: "Kadabra",
+                dateOfBirth: '03.10.2000',
+                aboutMe: 'this is text about me'
+            }
+
+            const { status, body } = await request(_httpServer)
+                .put('/users/profile')
+                .set('Authorization', `Bearer ${accessToken}`)
+                .send(dto)
+
+            expect(status).toBe(HttpStatus.OK)
+            expect(body.errors.length).toBe(0)
+
+            const findedUser = await manager.findOne(UserEntity, {
+                where: { id: user.id }
+            })
+
+            expect(findedUser.firstname).toBe(dto.firstname)
+            expect(findedUser.lastname).toBe(dto.lastname)
+            expect(findedUser.aboutMe).toBe(dto.aboutMe)
+            expect(findedUser.dateOfBirth.toISOString()).toBe(new Date(dto.dateOfBirth).toISOString())
+            expect(findedUser.username).toBe(dto.username)
+            expect(findedUser.updatedAt).not.toBe(null)
+        })
+
+        it('update user is failed email not confirmed', async () => {
+            const user = await testSeeder.createUser(testSeeder.getUserDto(), { emailConfirmed: false })
+            const accessToken = (await createJwtTokens(jwtService, user.id, 'asd')).accessToken
+
+            const dto: UpdateUserDto = {
+                username: user.username,
+                firstname: "Abra",
+                lastname: "Kadabra",
+                dateOfBirth: '03.10.2000',
+                aboutMe: 'this is text about me'
+            }
+
+            const { status, body } = await request(_httpServer)
+                .put('/users/profile')
+                .set('Authorization', `Bearer ${accessToken}`)
+                .send(dto)
+
+            expect(status).toBe(HttpStatus.OK)
+            expect(body.errors.length).toBe(1)
+
+            const findedUser = await manager.findOne(UserEntity, {
+                where: { id: user.id }
+            })
+
+            expect(findedUser.firstname).toBe(null)
+            expect(findedUser.lastname).toBe(null)
+            expect(findedUser.aboutMe).toBe(null)
+            expect(findedUser.dateOfBirth).toBe(null)
+            expect(findedUser.username).toBe(user.username)
+            expect(findedUser.updatedAt).toBe(null)
+        })
+
+        it('update user is failed username exist', async () => {
+            const user = await testSeeder.createUser(testSeeder.getUserDto())
+            const user2 = await testSeeder.createUser(testSeeder.getUserDto(2))
+            const accessToken = (await createJwtTokens(jwtService, user.id, 'asd')).accessToken
+
+            const dto: UpdateUserDto = {
+                username: user2.username,
+                firstname: "Abra",
+                lastname: "Kadabra",
+                dateOfBirth: '03.10.2000',
+                aboutMe: 'this is text about me'
+            }
+
+            const { status, body } = await request(_httpServer)
+                .put('/users/profile')
+                .set('Authorization', `Bearer ${accessToken}`)
+                .send(dto)
+
+            expect(status).toBe(HttpStatus.OK)
+            expect(body.errors.length).toBe(1)
+
+            const findedUser = await manager.findOne(UserEntity, {
+                where: { id: user.id }
+            })
+
+            expect(findedUser.firstname).toBe(null)
+            expect(findedUser.lastname).toBe(null)
+            expect(findedUser.aboutMe).toBe(null)
+            expect(findedUser.dateOfBirth).toBe(null)
+            expect(findedUser.username).toBe(user.username)
+            expect(findedUser.updatedAt).toBe(null)
+        })
+
+        it('update user is failed user year < 13', async () => {
+            const user = await testSeeder.createUser(testSeeder.getUserDto())
+            const accessToken = (await createJwtTokens(jwtService, user.id, 'asd')).accessToken
+
+            const dto: UpdateUserDto = {
+                username: user.username,
+                firstname: "Abra",
+                lastname: "Kadabra",
+                dateOfBirth: '17.04.2022',
+                aboutMe: 'this is text about me'
+            }
+
+            const { status, body } = await request(_httpServer)
+                .put('/users/profile')
+                .set('Authorization', `Bearer ${accessToken}`)
+                .send(dto)
+
+            expect(status).toBe(HttpStatus.OK)
+            expect(body.errors.length).toBe(1)
+
+            const findedUser = await manager.findOne(UserEntity, {
+                where: { id: user.id }
+            })
+
+            expect(findedUser.firstname).toBe(null)
+            expect(findedUser.lastname).toBe(null)
+            expect(findedUser.aboutMe).toBe(null)
+            expect(findedUser.dateOfBirth).toBe(null)
+            expect(findedUser.username).toBe(user.username)
+            expect(findedUser.updatedAt).toBe(null)
+        })
+    })
+})
