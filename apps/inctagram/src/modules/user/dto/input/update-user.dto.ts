28fe659109cadf65ce411e58f04f7f3b4ecaeae0
8blank8 <springfield.3298@gmail.com>
2024-04-17 15:17:47 +0700

    add: UpdateUserDto

---
+++ apps/inctagram/src/modules/user/dto/input/update-user.dto.ts
@@ -0,0 +1,31 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsOptional, IsString, Length, Matches, MaxLength, Validate } from "class-validator";
+
+export class UpdateUserDto {
+    @ApiProperty()
+    @IsString()
+    @Matches(/^[0-9A-Za-z_-]+$/)
+    @Length(6, 30)
+    username: string
+
+    @ApiProperty()
+    @Length(1, 50)
+    @Matches(/^[A-Za-zА-Яа-я]+$/)
+    firstname: string;
+
+    @ApiProperty()
+    @Length(1, 50)
+    @Matches(/^[A-Za-zА-Яа-я]+$/)
+    lastname: string;
+
+    @ApiProperty({ required: false })
+    @IsOptional()
+    @MaxLength(200)
+    aboutMe?: string;
+
+    @ApiProperty()
+    @Matches(/^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.\d{4}$/, {
+        message: 'Invalid date format. Use dd.mm.yyyy'
+    })
+    dateOfBirth: string;
+}
