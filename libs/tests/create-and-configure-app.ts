bb41b9a900ff483e7026bc27302a0ac897b7523b
8blank8 <springfield.3298@gmail.com>
2024-04-17 15:19:27 +0700

    update: createAndConfigureAppForTests

--- libs/tests/create-and-configure-app.ts
+++ libs/tests/create-and-configure-app.ts
@@ -1,7 +1,5 @@
-import * as cookieParser from 'cookie-parser';
 import { DataSource } from "typeorm";
 import { CreateTestModule } from "./create-test-module";
-import { BadRequestException, ValidationError, ValidationPipe } from '@nestjs/common';
 
 
 export const createAndConfigureAppForTests = async () => {
@@ -11,36 +9,9 @@ export const createAndConfigureAppForTests = async () => {
 
     const app = moduleRef.createNestApplication();
 
-    app.use(cookieParser())
-
-    app.useGlobalPipes(new ValidationPipe({
-        transform: true,
-        transformOptions: {
-            enableImplicitConversion: true,
-        },
-        exceptionFactory: (errors: ValidationError[]) => {
-            const errorsMessages = errors.map(e => {
-                let message = null;
-
-                if (e.constraints && Object.keys(e.constraints).length) {
-                    message = e.constraints[Object.keys(e.constraints)[0]]
-                }
-
-                return {
-                    field: e.property,
-                    message
-                }
-            })
-
-            throw new BadRequestException(errorsMessages)
-        }
-    }));
-
-    const httpServer = app.getHttpServer();
-
     const dataSource = await moduleRef.resolve(DataSource);
     const manager = dataSource.manager;
     const queryRunner = manager.connection.createQueryRunner();
 
-    return { moduleRef, app, httpServer, manager, queryRunner, dataSource }
+    return { moduleRef, app, manager, queryRunner, dataSource }
 }
