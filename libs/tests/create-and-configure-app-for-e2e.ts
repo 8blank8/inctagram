9f56a64d791e88ffba194e93171eae036e851f37
8blank8 <springfield.3298@gmail.com>
2024-04-17 15:19:17 +0700

    add: createAndConfigureAppForE2eTests

---
+++ libs/tests/create-and-configure-app-for-e2e.ts
@@ -0,0 +1,46 @@
+import * as cookieParser from 'cookie-parser';
+import { DataSource } from "typeorm";
+import { CreateModuleForControllerTest } from "./create-test-module";
+import { BadRequestException, ValidationError, ValidationPipe } from '@nestjs/common';
+
+
+export const createAndConfigureAppForE2eTests = async () => {
+
+    let moduleRef = await CreateModuleForControllerTest()
+        .compile();
+
+    const app = moduleRef.createNestApplication();
+
+    app.use(cookieParser())
+
+    app.useGlobalPipes(new ValidationPipe({
+        transform: true,
+        transformOptions: {
+            enableImplicitConversion: true,
+        },
+        exceptionFactory: (errors: ValidationError[]) => {
+            const errorsMessages = errors.map(e => {
+                let message = null;
+
+                if (e.constraints && Object.keys(e.constraints).length) {
+                    message = e.constraints[Object.keys(e.constraints)[0]]
+                }
+
+                return {
+                    field: e.property,
+                    message
+                }
+            })
+
+            throw new BadRequestException(errorsMessages)
+        }
+    }));
+
+    const httpServer = app.getHttpServer();
+
+    const dataSource = await moduleRef.resolve(DataSource);
+    const manager = dataSource.manager;
+    const queryRunner = manager.connection.createQueryRunner();
+
+    return { moduleRef, app, httpServer, manager, queryRunner, dataSource }
+}
