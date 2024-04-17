bc49905373260bdbd27c0406444fc9697aaef73e
8blank8 <springfield.3298@gmail.com>
2024-04-17 15:17:27 +0700

    add: UserController

---
+++ apps/inctagram/src/modules/user/controller/user.contoller.ts
@@ -0,0 +1,30 @@
+import { JwtAuthGuard } from "@libs/guards/jwt.guard";
+import { Body, Controller, Put, Req, UseGuards } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+import { UpdateUserCommand } from "../use-cases/update/dto/update-user.command";
+import { ReqWithUser } from "@libs/types/req-with-user";
+import { UpdateUserUseCase } from "../use-cases/update/update-user.use-case";
+import { UpdateUserDto } from "../dto/input/update-user.dto";
+
+
+@ApiTags('users')
+@Controller('users')
+export class UserContoller {
+
+    constructor(
+        private updateUserUseCase: UpdateUserUseCase,
+    ) { }
+
+    @UseGuards(JwtAuthGuard())
+    @Put('/profile')
+    async updateUserProfile(
+        @Body() dto: UpdateUserDto,
+        @Req() req: ReqWithUser
+    ) {
+        const command: UpdateUserCommand = {
+            ...dto,
+            userId: req.userId
+        }
+        return this.updateUserUseCase.execute(command)
+    }
+}
