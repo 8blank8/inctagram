0e26f2bdfc78305cacb95f0afd53f8fa07d6816d
8blank8 <springfield.3298@gmail.com>
2024-04-18 17:20:00 +0700

    update: tests for auth

--- apps/inctagram/src/modules/auth/contoller/__tests__/auth.contoller.spec.ts
+++ apps/inctagram/src/modules/auth/contoller/__tests__/auth.contoller.spec.ts
@@ -1,5 +1,5 @@
 import { HttpStatus, INestApplication } from "@nestjs/common"
-import { createAndConfigureAppForTests } from "../../../../../../../libs/tests/create-and-configure-app"
+import { createAndConfigureAppForTests } from "../../../../utils/test/create-and-configure-app"
 import { EntityManager, QueryRunner } from "typeorm"
 import { TestUtils } from "../../../../../../../libs/tests/test-utils"
 import * as request from 'supertest'
@@ -10,8 +10,8 @@ import { DeviceEntity } from "../../../device/entities/device.entity"
 import { CreateUserCommand } from "../../../user/use-cases/create/dto/create-user.command"
 import { ConfirmationUserCommand } from "../../use-cases/confirmation/dto/confirmation-user.command"
 import { ResetUserPasswordCommand } from "../../use-cases/recovery-password/dto/reset-user-password.command"
-import { createAndConfigureAppForE2eTests } from "@libs/tests/create-and-configure-app-for-e2e"
 import { appSetting } from "@libs/core/app-setting"
+import { createAndConfigureAppForE2eTests } from "@inctagram/src/utils/test/create-and-configure-app-for-e2e"
 
 
 describe('auth', () => {
