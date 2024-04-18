64c6ee304976d7067f64057ced8f375f3eab9809
8blank8 <springfield.3298@gmail.com>
2024-04-18 17:18:38 +0700

    add: CreateUserAvatarUseCase

---
+++ apps/files/src/modules/user/use-cases/create-avatar/create-user-avatar.use-case.ts
@@ -0,0 +1,80 @@
+import { Injectable } from "@nestjs/common";
+import { CreateUserAvatarCommand } from "./dto/create-user-avatar.command";
+import { UserRepository } from "../../repository/user.repository";
+import { Result } from "@libs/core/result";
+import { UserAvatarEntity } from "../../entities/user-avatar.entity";
+import { DataSource, EntityManager } from "typeorm";
+import { TransactionDecorator } from "@libs/infra/inside-transaction/inside-transaction";
+import { IdCreated } from "@libs/core/id-created";
+import { S3Service } from "@files/src/modules/s3/services/s3.service";
+
+
+@Injectable()
+export class CreateUserAvatarUseCase {
+
+    constructor(
+        private userRepo: UserRepository,
+        private dataSource: DataSource,
+        private s3Service: S3Service
+    ) { }
+
+    async execute(command: CreateUserAvatarCommand): Promise<Result<IdCreated>> {
+        const transaction = new TransactionDecorator(this.dataSource)
+
+        return transaction.doOperation(
+            command,
+            this.doOperation.bind(this)
+        )
+    }
+
+    async doOperation(
+        { dto, file, userId }: CreateUserAvatarCommand,
+        manager: EntityManager
+    ): Promise<Result<IdCreated>> {
+        try {
+            const user = await this.userRepo.getUserById(userId)
+            if (!user) return Result.Err(`user with id:${userId} not found`)
+
+            if (user.avatar) {
+                const isDelete = await this.s3Service.delete(user.avatar.url)
+                if (!isDelete.isSuccess) return Result.Err(isDelete.err.message)
+
+                const currentAvatarId = user.avatar.id
+
+                user.avatar = null
+                const userWithNonAvatar = await manager.save(user)
+                if (userWithNonAvatar.avatar) return Result.Err('user avatar is not deleted')
+
+                const isDeleteUserAvatar = await manager.delete(UserAvatarEntity, currentAvatarId)
+                if (isDeleteUserAvatar.affected !== 1) return Result.Err('avatar is not deleted')
+            }
+
+            const avatarUrl = this.createUrl(user.id, file.originalname)
+
+            const isUpload = await this.s3Service.upload(avatarUrl, file.buffer)
+            if (!isUpload.isSuccess) return Result.Err(isUpload.err.message)
+
+            const avatar = new UserAvatarEntity()
+            avatar.createdAt = new Date()
+            avatar.offsetX = dto.offsetX
+            avatar.offsetY = dto.offsetY
+            avatar.scale = dto.scale
+            avatar.user = user
+            avatar.url = avatarUrl
+
+            const createdAvatar = await manager.save(avatar)
+
+            user.avatar = createdAvatar
+            await manager.save(user)
+
+            return Result.Ok({ id: createdAvatar.id })
+        } catch (e) {
+            console.log(e)
+            return Result.Err('user avatar not created')
+        }
+    }
+
+    private createUrl(userId: string, fileName: string) {
+        return `${userId}/avatar/${fileName}`
+    }
+}
