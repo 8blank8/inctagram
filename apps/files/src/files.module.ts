82c50943f2c108da0de5b20d70ef58c12c411b80
8blank8 <springfield.3298@gmail.com>
2024-04-18 17:17:04 +0700

    update: FilesModule

--- apps/files/src/files.module.ts
+++ apps/files/src/files.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
+import { UserModule } from './modules/user/user.module';
+import { primaryPostgresConnectionOptions } from '@libs/infra/postgres-ormconfig';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CustomResultInterceptor } from '@libs/interceptor/custom-result.interceptor';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(primaryPostgresConnectionOptions),
+    UserModule,
+  ],
   controllers: [FilesController],
-  providers: [FilesService],
+  providers: [
+    FilesService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CustomResultInterceptor
+    },
+
+  ],
 })
-export class FilesModule {}
+export class FilesModule { }
