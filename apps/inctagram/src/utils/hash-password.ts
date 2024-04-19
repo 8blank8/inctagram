1ffbbf1b82c26cc7a8a3decd17a7617395e15c11
8blank8 <springfield.3298@gmail.com>
2024-04-19 16:34:56 +0700

    add: try catch

--- apps/inctagram/src/utils/hash-password.ts
+++ apps/inctagram/src/utils/hash-password.ts
@@ -1,12 +1,16 @@
 import { genSalt, hash } from "bcrypt"
 
 export const hashPassword = async (password: string, salt?: string) => {
-    if (!salt) salt = await genSalt(Number(process.env.SALT_ROUNDS) || 10)
+    try {
+        if (!salt) salt = await genSalt(Number(process.env.SALT_ROUNDS) || 10)
 
-    const passwordHash = await hash(password, salt)
+        const passwordHash = await hash(password, salt)
 
-    return {
-        passwordHash,
-        passwordSalt: salt
+        return {
+            passwordHash,
+            passwordSalt: salt
+        }
+    } catch (e) {
+        console.log(e)
     }
 }
