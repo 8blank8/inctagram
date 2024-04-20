9903587b6810a49433775ff5c42307a231498537
8blank8 <springfield.3298@gmail.com>
2024-04-21 00:04:36 +0700

    update: refirect url for goole

--- apps/inctagram/src/modules/auth/contoller/auth.controller.ts
+++ apps/inctagram/src/modules/auth/contoller/auth.controller.ts
@@ -24,6 +24,7 @@ import { RefreshTokenCommand } from "../use-cases/refresh-token/dto/refresh-toke
 import { CreateUserGoogleOauthUseCase } from "../../user/use-cases/create/create-user-google-ouath.use-case";
 import { CreateUserGoogleOauthCommand } from "../../user/use-cases/create/dto/create-user-google-ouath.command";
 import { GoogleOAuthGuard } from "@libs/guards/google.guard";
+import { appSetting } from "@libs/core/app-setting";
 
 @ApiTags('auth')
 @Controller('auth')
@@ -164,10 +165,10 @@ export class AuthContoller {
         }
 
         const result = await this.createUserGoogleOauthUseCase.execute(command)
-        if (!result.isSuccess) return res.status(HttpStatus.BAD_REQUEST).redirect(`http://localhost:3000/`)
+        if (!result.isSuccess) return res.status(HttpStatus.BAD_REQUEST).redirect(`${appSetting.FRONT_URL}`)
 
         return res
             .cookie('refreshToken', result.value.refreshToken, { httpOnly: true, secure: true })
-            .redirect('http://localhost:3000/')
+            .redirect(`${appSetting.FRONT_URL}`)
     }
 }
