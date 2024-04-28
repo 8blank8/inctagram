e9b7ca68ad844de07fd33930e50cc062d120b012
8blank8 <springfield.3298@gmail.com>
2024-04-28 14:41:31 +0700

    update: registration use case

--- apps/inctagram/src/modules/auth/use-cases/registration/registration-user.use-case.ts
+++ apps/inctagram/src/modules/auth/use-cases/registration/registration-user.use-case.ts
@@ -3,102 +3,23 @@ import { RegistrationUserCommand } from "./dto/registration-user.command";
 import { Result } from "../../../../../../../libs/core/result";
 import { IdCreated } from "../../../../../../../libs/core/id-created";
 import { MailService } from "../../../../../../../libs/mailer/mailer.service";
-import { TransactionDecorator } from "@libs/infra/inside-transaction/inside-transaction";
-import { DataSource, EntityManager } from "typeorm";
-import { UserRepository } from "@inctagram/src/modules/user/repository/user.repository";
-import { hashPassword } from "@inctagram/src/utils/hash-password";
-import { v4 as uuid } from 'uuid'
-import { UserEntity } from "@libs/infra/entities/user.entity";
+import { CreateUserUseCase } from "@inctagram/src/modules/user/use-cases/create/create-user.use-case";
 
 
 @Injectable()
 export class RegistrationUserUseCase {
     constructor(
         private mailService: MailService,
-        private dataSource: DataSource,
-        private userRepo: UserRepository
+        private createUserUseCase: CreateUserUseCase
     ) { }
 
     async execute(command: RegistrationUserCommand): Promise<Result<IdCreated>> {
 
-        const transaction = new TransactionDecorator(this.dataSource)
+        const user = await this.createUserUseCase.execute(command)
+        if (!user.isSuccess) return Result.Err(user.err.message)
 
-        return transaction.doOperation(
-            command,
-            this.doOperation.bind(this)
-        );
-    }
+        await this.mailService.sendEmailConfirmationMessage(user.value.email, user.value.username, user.value.confirmationCode)
 
-    async doOperation(
-        { email, password, username }: RegistrationUserCommand,
-        manager: EntityManager
-    ): Promise<Result<IdCreated>> {
-
-        try {
-
-            const findedUserEmail = await this.userRepo.getUserByEmail(email)
-            if (findedUserEmail && findedUserEmail.emailConfirmed) return Result.Err('user with email is exist')
-
-            const findedUserUsername = await this.userRepo.getUserByUsername(username)
-            if (findedUserUsername && findedUserUsername.emailConfirmed) return Result.Err('user with username is exist')
-
-            const { passwordHash, passwordSalt } = await hashPassword(password)
-
-            let createdUser: UserEntity
-
-            if (findedUserEmail && !findedUserEmail.emailConfirmed) {
-                createdUser = await this.createUser(
-                    findedUserEmail,
-                    email,
-                    username,
-                    passwordHash,
-                    passwordSalt,
-                    manager
-                )
-            }
-
-            if (findedUserUsername && !findedUserUsername.emailConfirmed) {
-                createdUser = await this.createUser(
-                    findedUserUsername,
-                    email,
-                    username,
-                    passwordHash,
-                    passwordSalt,
-                    manager
-                )
-            }
-
-            if (!createdUser) {
-                const user = new UserEntity()
-
-                createdUser = await this.createUser(
-                    user,
-                    email,
-                    username,
-                    passwordHash,
-                    passwordSalt,
-                    manager
-                )
-            }
-            await this.mailService.sendEmailConfirmationMessage(email, username, createdUser.confirmationCode)
-
-            return Result.Ok({ id: createdUser.id })
-
-        } catch (e) {
-            console.log(e)
-            return Result.Err('CreateUserUseCase error')
-        }
-    }
-
-    async createUser(user: UserEntity, email: string, username: string, passwordHash: string, passwordSalt: string, manager: EntityManager): Promise<UserEntity> {
-
-        user.username = username
-        user.email = email
-        user.createdAt = new Date()
-        user.passwordHash = passwordHash
-        user.passwordSalt = passwordSalt
-        user.confirmationCode = uuid()
-
-        return manager.save(user)
+        return Result.Ok({ id: user.value.id })
     }
 }
