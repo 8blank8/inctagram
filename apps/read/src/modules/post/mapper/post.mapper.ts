2a2f1221702370a2433a2de682373aca9132d75c
8blank8 <springfield.3298@gmail.com>
2024-05-01 18:22:11 +0700

    update: PostMapper methods

--- apps/read/src/modules/post/mapper/post.mapper.ts
+++ apps/read/src/modules/post/mapper/post.mapper.ts
@@ -1,13 +1,37 @@
 import { PostEntity } from "@libs/infra/entities/post.entity";
 import { PostsViewDto } from "../dto/posts-view.dto";
+import { PostsForPublicViewDto } from "../dto/posts-for-public-view.dto";
+import { UserMapper } from "../../user/mapper/user.mapper";
+import { appSetting } from "@libs/core/app-setting";
 
 
 export class PostMapper {
-    static fromPostToPostsViewDto(post: PostEntity): PostsViewDto {
+    static fromPostToPostsViewDto(post: PostEntity): PostsViewDto[] {
+
+        return post.photos.map(p => {
+            return {
+                postId: post.id,
+                url: appSetting.AWS_S3_BASE_URL + p.url,
+                aspectRatio: p.aspectRatio,
+                offsetX: p.offsetX,
+                offsetY: p.offsetY,
+                scale: p.scale,
+                cursor: post.cursor
+            }
+        })
+    }
+
+    static fromPostToPublicPostViewDto(post: PostEntity): PostsForPublicViewDto {
         return {
             id: post.id,
-            url: post.photos[0].url,
-            cursor: post.cursor
+            createdAt: post.createdAt.toISOString(),
+            description: post.description,
+            user: {
+                id: post.user.id,
+                username: post.user.username,
+                avatar: UserMapper.fromAvatarToAvatarViewDto(post.user.avatar)
+            },
+            photos: PostMapper.fromPostToPostsViewDto(post),
         }
     }
 }
