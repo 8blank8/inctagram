b82066daa00decee084381c6c1a6b5c331a48fcc
8blank8 <springfield.3298@gmail.com>
2024-05-01 18:22:27 +0700

    add: getPublicPosts for post query repo

--- apps/read/src/modules/post/repositories/post-query.repository.ts
+++ apps/read/src/modules/post/repositories/post-query.repository.ts
@@ -7,6 +7,7 @@ import { Paginated } from "@libs/core/pagination";
 import { PostsViewDto } from "../dto/posts-view.dto";
 import { PostMapper } from "../mapper/post.mapper";
 import { PostEntity } from "@libs/infra/entities/post.entity";
+import { PostsForPublicViewDto } from "../dto/posts-for-public-view.dto";
 
 
 @Injectable()
@@ -42,12 +43,39 @@ export class PostQueryRepository {
                 Paginated.new({
                     count: posts[1],
                     size: size,
-                    items: posts[0].map(p => PostMapper.fromPostToPostsViewDto(p))
+                    items: posts[0].map(p => PostMapper.fromPostToPostsViewDto(p)[0])
                 })
             )
         } catch (e) {
             console.log(e)
             return Result.Err('get posts some error')
+        }
+    }
+
+    async getPublicPosts(): Promise<Result<PostsForPublicViewDto[]>> {
+        try {
+
+            const posts = await this.postRepo.find({
+                where: {
+                    public: true
+                },
+                relations: {
+                    user: {
+                        avatar: true
+                    },
+                    photos: true,
+                },
+                order: { createdAt: 'DESC' },
+                take: 4
+            })
+
+            return Result.Ok(
+                posts.map(p => PostMapper.fromPostToPublicPostViewDto(p))
+            )
+
+        } catch (e) {
+            console.log('getPublicPosts', e)
+            return Result.Err('some error get public posts')
         }
     }
 }
