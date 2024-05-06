fdc13abd0ccded6716a045cc33359350c2eae258
8blank8 <springfield.3298@gmail.com>
2024-05-06 12:16:51 +0700

    add: enums

---
+++ libs/enum/enum.ts
@@ -0,0 +1,10 @@
+export enum PaymentSystenType {
+    STRIPE = 'STRIPE',
+    PAYPALL = 'PAYPALL'
+}
+
+export enum TermSubscriptionType {
+    ONE_DAY = 'ONE_DAY',
+    SEVEN_DAYS = 'SEVEN_DAYS',
+    ONE_MONTH = 'ONE_MONTH'
+}
