cb4c3bc18ea314e579334b6c08fcefdc04268225
8blank8 <springfield.3298@gmail.com>
2024-05-06 12:15:59 +0700

    update: user contoller added endpoint payment

--- apps/inctagram/src/modules/user/controller/user.contoller.ts
+++ apps/inctagram/src/modules/user/controller/user.contoller.ts
@@ -1,10 +1,14 @@
 import { JwtAuthGuard } from "@libs/guards/jwt.guard";
-import { Body, Controller, Put, Req, UseGuards } from "@nestjs/common";
+import { Body, Controller, HttpStatus, Post, Put, Req, Res, UseGuards } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { UpdateUserCommand } from "../use-cases/update/dto/update-user.command";
 import { ReqWithUser } from "@libs/types/req-with-user";
 import { UpdateUserUseCase } from "../use-cases/update/update-user.use-case";
 import { UpdateUserDto } from "../dto/update-user.dto";
+import { PaymentSubscriptionDto } from "../dto/payment-subscription.dto";
+import { PaymentSubscriptionCommand } from "../use-cases/pyament/dto/payment-subscription.command";
+import { PaymentSubscriptionUseCase } from "../use-cases/pyament/payment-subscription.use-case";
+import { Response } from "express";
 
 
 @ApiTags('users')
@@ -13,6 +17,7 @@ export class UserContoller {
 
     constructor(
         private updateUserUseCase: UpdateUserUseCase,
+        private paymentSubscriptionUseCase: PaymentSubscriptionUseCase
     ) { }
 
     @UseGuards(JwtAuthGuard())
@@ -26,5 +31,28 @@ export class UserContoller {
             userId: req.userId
         }
         return this.updateUserUseCase.execute(command)
+    }
+
+    @UseGuards(JwtAuthGuard())
+    @Post('/payment-subscription')
+    async paymentSubscription(
+        @Req() req: ReqWithUser,
+        @Body() dto: PaymentSubscriptionDto,
+        @Res() res: Response
+    ) {
+        const command: PaymentSubscriptionCommand = {
+            ...dto,
+            userId: req.userId
+        }
+
+        const result = await this.paymentSubscriptionUseCase.execute(command)
+
+        if (!result.isSuccess) return res.status(HttpStatus.BAD_REQUEST).send({
+            resultCode: 1,
+            data: {},
+            errors: [result.err]
+        })
+
+        return res.redirect(result.value.url)
     }
 }
