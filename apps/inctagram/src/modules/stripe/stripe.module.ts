4b3224ed8694663ad02af435a62006a19092dd8a
8blank8 <springfield.3298@gmail.com>
2024-05-08 11:51:22 +0700

    update: stipe module added dependecies

--- apps/inctagram/src/modules/stripe/stripe.module.ts
+++ apps/inctagram/src/modules/stripe/stripe.module.ts
@@ -1,15 +1,28 @@
 import { Module } from "@nestjs/common";
 import { StripeService } from "./services/stripe.service";
 import { StripeContoller } from "./contoller/stripe.contoller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { SubscriptionEntity } from "@libs/infra/entities/subscription.entity";
+import { UserModule } from "../user/user.module";
+import { JwtService } from "@nestjs/jwt";
+import { PaymentSubscriptionUseCase } from "./use-cases/create/payment-subscription.use-case";
+import { SuccessPaymentSubscriptionUseCase } from "./use-cases/success-payment/success-payment-subscription.use-case";
 
 
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([SubscriptionEntity]),
+        UserModule
+    ],
     controllers: [
         StripeContoller
     ],
     providers: [
-        StripeService
+        StripeService,
+        PaymentSubscriptionUseCase,
+        SuccessPaymentSubscriptionUseCase,
+        JwtService
     ],
     exports: [
         StripeService
