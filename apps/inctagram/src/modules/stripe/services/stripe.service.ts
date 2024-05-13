b0c45131a50a29a7735076bf0e27f5f8afaa04b1
8blank8 <springfield.3298@gmail.com>
2024-05-13 11:34:47 +0700

    add: method cancel subscription for stripe service

--- apps/inctagram/src/modules/stripe/services/stripe.service.ts
+++ apps/inctagram/src/modules/stripe/services/stripe.service.ts
@@ -15,9 +15,13 @@ export class StripeService {
         this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
     }
 
-    private async createPayment(unit_amount: number, description: string, userId: string, termSubscriptionType: TermSubscriptionType) {
-        const session = await this.stripe.checkout.sessions.create({
-            success_url: 'https://example.com/success',
+    async cancelSubscription(subscriptionId: string) {
+        return this.stripe.subscriptions.cancel(subscriptionId)
+    }
+
+    private async createPayment(unit_amount: number, description: string, userId: string, termSubscriptionType: TermSubscriptionType, isSubscription: boolean = false) {
+
+        const options: Stripe.Checkout.SessionCreateParams = {
             line_items: [
                 {
                     price_data: {
@@ -28,36 +32,47 @@ export class StripeService {
                             description: description,
                         },
                     },
-                    quantity: 1,
+                    quantity: 1
                 },
             ],
-            mode: 'subscription',
-            currency: 'usd',
+            success_url: 'https://example.com/success',
+            mode: 'payment',
             metadata: {
                 userId,
                 termSubscriptionType
             },
+        }
 
-        });
+        if (isSubscription) {
+            options.line_items[0].price_data.recurring = {
+                interval: termSubscriptionType,
+            }
+            options.mode = 'subscription'
+            options.subscription_data = {
+                billing_cycle_anchor: Math.round(Date.now() / 1000) + 10,
+            }
+        }
+
+        const session = await this.stripe.checkout.sessions.create(options);
 
         return session
     }
 
-    async paymentSubscription(term: TermSubscriptionType, userId: string): Promise<Result<Stripe.Response<Stripe.Checkout.Session>>> {
+    async paymentSubscription(term: TermSubscriptionType, userId: string, isSubscription: boolean): Promise<Result<Stripe.Response<Stripe.Checkout.Session>>> {
         try {
             let session: Stripe.Response<Stripe.Checkout.Session>
 
             switch (term) {
                 case TermSubscriptionType.ONE_DAY: {
-                    session = await this.createPayment(10, 'subscription on 1 day', userId, term)
+                    session = await this.createPayment(10, 'subscription on 1 day', userId, term, isSubscription)
                     break
                 }
                 case TermSubscriptionType.SEVEN_DAYS: {
-                    session = await this.createPayment(50, 'subscription on 7 days', userId, term)
+                    session = await this.createPayment(50, 'subscription on 7 days', userId, term, isSubscription)
                     break
                 }
                 case TermSubscriptionType.ONE_MONTH: {
-                    session = await this.createPayment(100, 'subscription on 1 month', userId, term)
+                    session = await this.createPayment(100, 'subscription on 1 month', userId, term, isSubscription)
                     break
                 }
             }
