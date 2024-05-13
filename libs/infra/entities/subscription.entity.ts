a9e5e1abb595648d4b292fbd9889fe7d11e8c4ff
8blank8 <springfield.3298@gmail.com>
2024-05-13 11:37:32 +0700

    add: columns isSubscription, subscriptionId and isActive for SubscriptionEntity

--- libs/infra/entities/subscription.entity.ts
+++ libs/infra/entities/subscription.entity.ts
@@ -15,6 +15,15 @@ export class SubscriptionEntity extends BaseEntity {
     @Column({ enum: PaymentSystenType, type: 'enum' })
     paymentSystem: PaymentSystenType
 
+    @Column({ default: false })
+    isSubscription: boolean
+
+    @Column({ nullable: true })
+    subscriptionId: string
+
+    @Column({ default: true })
+    isActive: boolean
+
     @Column({ type: 'timestamp without time zone' })
     expirationDate: Date
 
